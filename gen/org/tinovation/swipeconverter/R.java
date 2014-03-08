d87dea4785366448dc58dec15c992850b6c7bfa5
suryaruddaraju <surya.ruddaraju@gmail.com>
2014-03-08 15:14:57 -0800

    updated arrays

--- gen/org/tinovation/swipeconverter/R.java
+++ gen/org/tinovation/swipeconverter/R.java
@@ -10,6 +10,7 @@ package org.tinovation.swipeconverter;
 public final class R {
     public static final class array {
         public static final int Distance=0x7f040001;
+        public static final int Mass=0x7f040002;
         public static final int Temperature=0x7f040000;
     }
     public static final class attr {
