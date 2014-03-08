2443e6bda9e47f2fe6ece932b568171cd119d35e
Hari Dhanushkodi <hari97.nd@gmail.com>
2014-03-08 14:19:53 -0800

    Initial commit

---
+++ src/org/tinovation/swipeconverter/TitleActivity.java
@@ -0,0 +1,28 @@
+package org.tinovation.swipeconverter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class TitleActivity extends Activity {
+	
+	protected Spinner type;
+	protected Spinner units;
+	protected EditText number;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_title);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.title, menu);
+		return true;
+	}
+
+}
